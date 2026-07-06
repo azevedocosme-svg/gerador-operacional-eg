595fac543b799fc48831044c2eae220bdad4908d
azevedocosme-svg <azevedo.cosme@gmail.com>
2026-07-06 09:56:03 -0300

    Update app.py
    Retorno interface principal SIGOT

--- app.py
+++ app.py
@@ -3,3 +3,24 @@ import pandas as pd
 import re
 
 from regras_operacionais import *
+
+st.set_page_config(page_title="SIGOT", layout="wide")
+
+st.title("🚀 SIGOT – Sistema Inteligente de Gestão Operacional de Transportes")
+
+arquivo = st.file_uploader(
+    "📥 Importar Relatório Excel",
+    type=["xlsx"]
+)
+
+if arquivo:
+
+    df = pd.read_excel(arquivo)
+
+    st.success("Arquivo carregado com sucesso!")
+
+    st.subheader("📋 COLUNAS ENCONTRADAS")
+    st.write(list(df.columns))
+
+    st.subheader("📊 Prévia do Excel")
+    st.dataframe(df.head())
