f1f741f8dfc544c6bf8c979a3f89d563ef9a0a2f
azevedocosme-svg <azevedo.cosme@gmail.com>
2026-07-06 11:19:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import streamlit as st
+import pandas as pd
+import re
+
+from regras_operacionais import *
+
+# ==================================================
+# CONFIGURAÇÃO
+# ==================================================
+
+st.set_page_config(
+    page_title="SIGOT",
+    layout="wide"
+)
+
 # ==================================================
 # IDENTIFICA CATEGORIA OPERACIONAL
 # ==================================================
@@ -15,3 +30,71 @@ def identificar_categoria(tipo_veiculo):
                 return categoria
 
     return "❓ OUTROS"
+
+# ==================================================
+# TÍTULO
+# ==================================================
+
+st.title("🚀 SIGOT")
+st.subheader("Sistema Inteligente de Gestão Operacional de Transportes")
+
+# ==================================================
+# IMPORTAÇÃO
+# ==================================================
+
+arquivo = st.file_uploader(
+    "📥 Importar Relatório Excel",
+    type=["xlsx"]
+)
+
+# ==================================================
+# PROCESSAMENTO
+# ==================================================
+
+if arquivo:
+
+    df = pd.read_excel(arquivo)
+
+    st.success("✅ Arquivo carregado com sucesso")
+
+    st.subheader("📋 COLUNAS ENCONTRADAS")
+    st.write(df.columns.tolist())
+
+    # ==============================================
+    # CATEGORIA OPERACIONAL
+    # ==============================================
+
+    df["Categoria Operacional"] = df["Tipo de Veículo"].apply(
+        identificar_categoria
+    )
+
+    # ==============================================
+    # DASHBOARD
+    # ==============================================
+
+    st.markdown("---")
+
+    st.markdown("# 📊 RESUMO POR CATEGORIA")
+
+    resumo = (
+        df["Categoria Operacional"]
+        .value_counts()
+        .reset_index()
+    )
+
+    resumo.columns = [
+        "Categoria",
+        "Quantidade"
+    ]
+
+    st.table(resumo)
+
+    # ==============================================
+    # PRÉVIA
+    # ==============================================
+
+    st.markdown("---")
+
+    st.subheader("📊 Prévia do Excel")
+
+    st.dataframe(df.head())
